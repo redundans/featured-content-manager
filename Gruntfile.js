badce73b930c7521f2634eed097842865455d7d4
Jesper Nilsson <jesper@klandestino.se>
2015-12-02 14:17:29 +0100

    Adds grunt to project.

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+ 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    banner: '/*! <%= pkg.name %> <%= pkg.version %> filename.min.css <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */\n',
+                    style: 'compressed'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'scss',
+                    src: [
+                        'admin/assets/sass/*.scss'
+                    ],
+                    dest: 'admin/assets/css',
+                    ext: '.min.css'
+                }]
+            }
+        },
+        uglify: {
+            dist: {
+                options: {
+                    banner: '/*! <%= pkg.name %> <%= pkg.version %> filename.min.js <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */\n',
+                },
+                files: {
+                    'admin/assets/js/customizer.min.js' : [
+                        'admin/assets/js/customizer.js'
+                    ]
+                }
+            }
+        },
+        watch: {
+            all: {
+                files: 'scss/*.scss',
+                tasks: ['sass'],
+            },
+        },
+    });
+ 
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+ 
+    grunt.registerTask('default', [
+        'sass:dist',
+        'uglify:dist'
+    ]);
+ 
+};
