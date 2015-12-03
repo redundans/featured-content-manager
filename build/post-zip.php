6ff42d0234014de5c3f3c8c50cc2fc401ea41e20
Alexander Berthelsen <alexander.berthelsen@gmail.com>
2015-12-03 14:54:09 +0100

    make build script commit changed files as well as tag release

--- build/post-zip.php
+++ build/post-zip.php
@@ -1,22 +1,9 @@
 <?php
-// echo "Upload plugin to Klandestino Plugins? (yes/no) ";
-// 	if ( 'yes' == trim( fgets( STDIN ) ) ) {
-// 		echo "What's your username on the plugin server? ";
-// 		if ( $username = trim( fgets( STDIN ) ) ) {
-// 				echo "Uploading... \n";
-// 				chdir( $parent_dir );
-// 				system( 'scp readme.txt ' . $username . '@skalman.klandestino.se:/home/' . $username );
-// 				chdir( $build_cfg_dir );
-// 				system( 'scp ' . $zip_name . ' ' . $username . '@skalman.klandestino.se:/home/' . $username );
-// 				system( 'ssh ' . $username . '@skalman.klandestino.se');
-// 				system( 'sudo mv readme.txt /var/www/plugins.klandestino.se/wp-content/uploads/2015/11' );
-// 				echo "All done, remember to update the version number on plugins.klandestino.se as well! \n";
-// 		}
-// 	} else {
-// 		echo "Alright, then you have to do it yourself\n";
-// 	}
 echo "Tag version on Github? (yes/no) ";
 	if ( 'yes' == trim( fgets( STDIN ) ) ) {
+		system( 'git add -A' );
+		system( "git commit -m 'Deploying version $version'" );
+		system( 'git push origin master' );
 		system( "git tag $version" );
 		system( 'git push origin --tags' );
 	}
