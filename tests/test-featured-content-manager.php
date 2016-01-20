60268d499722f46ba1ddfc4a3cc2d1ac70c594a9
Tom Baro <tom@klandestino.se>
2016-01-20 16:09:27 +0100

    Test that get_children returns the provided id

--- tests/test-featured-content-manager.php
+++ tests/test-featured-content-manager.php
@@ -82,6 +82,22 @@ class Featured_Content_Tests extends WP_UnitTestCase {
 	}
 
 	/**
+	 * @covers Featured_Content_Manager::get_children
+	 */
+	function test_get_children_query_contains_correct_parent_id() {
+		// Arrange
+		$post_id = 2;
+
+		// Act
+		$query = $this->plugin->get_children( $post_id );
+
+		// Assert
+		var_dump($query);
+		$this->assertObjectHasAttribute( 'posts', $query );
+		$this->assertEquals( 2, $query->query['post_parent'] );
+	}
+
+	/**
 	 * @covers Featured_Content_Manager::get_plugin_slug
 	 */
 	function test_get_plugin_slug_returns_slug() {
